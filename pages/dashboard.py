50fc56eb498a146bc62989004def4e4aeb22ef7d
Siddhant <vishalsahu258@gmail.com>
2024-01-08 23:38:05 +0530

    changes

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -5,13 +5,12 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from plotly.offline import init_notebook_mode
+from plotly.offline import make_subplots
 import seaborn as sns
 import datetime as dt
 import warnings
 warnings.filterwarnings('ignore')
 pd.set_option('display.max_columns',None)
-init_notebook_mode(connected=True)
 
 st.markdown(
     """
